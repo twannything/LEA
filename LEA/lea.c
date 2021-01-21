c61f79c0bc61f6b862bd64e5df5aefae550f106e
김태완 <ktw0308@kookmin.kr>
2021-01-21 12:27:23 +0900

    12:27

--- LEA/lea.c
+++ LEA/lea.c
@@ -1,18 +1,18 @@
 #pragma once
 #include "lea.h"
 
-unsigned int delta[8] = { 0xc3efe9db, 0x44626b02, 0x79e27c8a, 0x78df30ec, 0x715ea49e, 0xc785da0a, 0xe04ef22a, 0xe5c40957 };
-
+unsigned int delta[8] = { 0xc3efe9db, 0x44626b02, 0x79e27c8a, 0x78df30ec, 0x715ea49e, 0xc785da0a, 0xe04ef22a, 0xe5c40957
+};
 void enc_lea_128(unsigned char* p, unsigned int* rk,unsigned char* c) {
 	unsigned int x[4] = { NULL };
 	unsigned int tmp[4] = { NULL };
 
 	memcpy(x, p, 16);
 	for (int i = 0; i < nr; i++) {
-		round_enc_128(x, rk,i,tmp);
-		memcpy(x, tmp,4);
+		round_enc_128(x, rk+(6*i),i,tmp);
+		memcpy(x, tmp,16);
 	}
-	memcpy(c, x, 4);
+	memcpy(c, x, 16);
 }
 
 void dec_lea_128(unsigned int* c, unsigned int* rk, unsigned int* p) {
@@ -37,9 +37,9 @@ unsigned int ROR(unsigned int x, unsigned int i) {
 }
 
 void round_enc_128(unsigned int* x, unsigned int* rk, unsigned int i, unsigned int* y) {
-	*(y) = ROL(((*x) ^ (*rk)), 9) + *(x + 1) ^ *(rk + 1);
-	*(y + 1) = ROR(*(x + 1) ^ *(rk + 2), 5) + *(x + 2) ^ *(rk + 3);
-	*(y + 2) = ROR(*(x + 2) ^ *(rk + 4), 3) + *(x + 3) ^ *(rk + 5);
+	*(y) = ROL(((*x) ^ (*rk)) + (*(x + 1) ^ *(rk + 1)),9);
+	*(y + 1) = ROR((*(x + 1) ^ *(rk + 2)) + ((*(x + 2) ^ *(rk + 3))),5);
+	*(y + 2) = ROR((*(x + 2) ^ *(rk + 4)) + ((*(x + 3) ^ *(rk + 5))),3);
 	*(y + 3) = *x;
 }
 
@@ -55,10 +55,10 @@ void enc_key_schedule_128(unsigned char* k, unsigned int* rk) {
 	unsigned int T[4] = { NULL };
 	memcpy(T, k, 16);
 	for (unsigned int i = 0; i < 24; i++) {
-		T[0] = ROL(T[0] + ROL(delta[i % 4], i), 1);
-		T[1] = ROL(T[1] + ROL(delta[i % 4], i + 1), 3);
-		T[2] = ROL(T[2] + ROL(delta[i % 4], i + 2), 6);
-		T[3] = ROL(T[3] + ROL(delta[i % 4], i + 3), 11);
+		T[0] = ROL((T[0] + ROL(delta[i % 4], i)), 1);
+		T[1] = ROL((T[1] + ROL(delta[i % 4], i + 1)), 3);
+		T[2] = ROL((T[2] + ROL(delta[i % 4], i + 2)), 6);
+		T[3] = ROL((T[3] + ROL(delta[i % 4], i + 3)), 11);
 		*(rk + (i*6)+0) = T[0];
 		*(rk + (i * 6) + 1) = T[1];
 		*(rk + (i * 6) + 2) = T[2];
--- LEA/main.c
+++ LEA/main.c
@@ -1,14 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include "lea.h"
+#include <time.h>
 
-unsigned char k_128[16] = { 0x0f, 0x01, 0x2d, 0x3c, 0x4b, 0x5a, 0x69, 0x78, 0x87, 0x96, 0xa5, 0xb4, 0xc3, 0xd2, 0xe1, 0xf0 };
+unsigned char k_128[16] = { 0x0f, 0x1e, 0x2d, 0x3c, 0x4b, 0x5a, 0x69, 0x78, 0x87, 0x96, 0xa5, 0xb4, 0xc3, 0xd2, 0xe1, 0xf0 };
 unsigned char p[16] = { 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f };
 unsigned char c[16] = { NULL };
 
 void main() {
 	unsigned int rk[144] = { NULL };
-	enc_key_schedule_128(k_128, rk);
-	enc_lea_128(p, rk, c);
-	printf("%02x",c[0]);
+	unsigned int tmp[4] = { NULL };
+	unsigned int ttmp[4] = { NULL };
+	int i = 0;
+	clock_t start = clock();
+	while (i < 100) {
+		i++;
+		enc_key_schedule_128(k_128, rk);
+		enc_lea_128(p, rk, c);
+	}
+	clock_t end = clock();
+
+	float res1 = (float)(end - start) / CLOCKS_PER_SEC;
+	printf("%lf\n", res1);
+	for(int i =0; i<16; i++) printf("%02x ",c[i]);
+	printf("\n");
 }
